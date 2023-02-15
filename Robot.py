e844f5e2e009553bf2fe087425e671d22a27eb42
Kevin Ma <115964136+kevindangkhoama@users.noreply.github.com>
2023-02-15 13:26:42 -0500

    Update Robot.py
    Robot.py is fully functional now. Fixed type errors. Encrypted config is saved as a text file in the home directory.

--- Robot.py
+++ Robot.py
@@ -7,11 +7,15 @@ from nacl.public import PrivateKey, PublicKey, Box
 
 
 def Encrypter(user_public):
-    # WireGuard private key
-    wg_private = ""
-    # Convert byte string to Private Key object
+    # Decode and assign wg_private as a Private Key object
+    wg_private = "Zlx9NZ4sW+WmdHWi7gN0Js6SabYeDQJL6yKOt7XVVjE="
+    wg_private = base64.b64decode(wg_private)
     wg_private = nacl.public.PrivateKey(wg_private)
     
+    # Decode User and assign user_public as a Public Key object
+    user_public = base64.b64decode(user_public)
+    user_public = nacl.public.PublicKey(user_public)
+
     # Create a WireGuard Box
     wg_box = Box(wg_private, user_public)
     
@@ -23,21 +27,27 @@ def Encrypter(user_public):
     message = f"USER_IP={user_ip} | PSK={user_psk}"
     # print(message) 
     
-    # Encrypt IP and PSK in base64 and return
+    # Encrypt IP and PSK and return
     encrypted = base64.b64encode(wg_box.encrypt(message.encode()))
-    return encrypted
+    
+    # Export to text file
+    home_dir = os.path.expanduser('~')
+    with open(os.path.join(home_dir, 'Encrypted_Config.txt'), 'wb') as fp:
+        fp.write(encrypted)
 
 
 # Command Line Arguments
 if len(sys.argv) == 2:
-    # Open User Public Key
-    with open('User_Public.txt', 'rb') as f:
-        data = f.read()
-        byte_string = data.decode('UTF-8').strip()
-        user_public = byte_string
-        # Convert byte string to Public Key Object
-        user_public = nacl.public.PublicKey(user_public)
+    print("Encrypting...")
+    
+    # Open text file and store as a variable
+    with open('User_Public.txt', 'r') as fp:
+        user_public = fp.read()
+        
+    # Run Encrypter    
     Encrypter(user_public)
+    print("Done")
+    
 else:
     # Invalid Command
-    print("Usage: Robot.py <User_Public.txt>", file=sys.stderr)
+    print("Usage: Robot.py <User_Public.txt>", file=sys.stderr) 
