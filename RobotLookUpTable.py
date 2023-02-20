50453e6c977c169ce1097039417e0298e1038cfc
Kevin Ma <115964136+kevindangkhoama@users.noreply.github.com>
2023-02-20 13:56:33 -0500

    Delete RobotLookUpTable.py

--- RobotLookUpTable.py
+++
@@ -1,6 +0,0 @@
-# Format: 'Robot' : ["Private Key", "Public Key"],
-robot_table = {
-    'Robot1': ['Zlx9NZ4sW+WmdHWi7gN0Js6SabYeDQJL6yKOt7XVVjE=', 'dnBijRHCphJpBoNxFuflGQqpWS8pWbnOBlPLQHNn3H0='],
-    'Robot2': ['Private Key2', 'Public Key2'],
-    'Robot3': ['Private Key3', 'Public Key3']
-}
